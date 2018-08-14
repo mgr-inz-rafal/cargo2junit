602a3ce20796f9113d5e0dfdd97037a14de7fcca
mgr_inz_rafal <rchabowski@gmail.com>
2018-08-14 21:43:25 +0200

    Reading input file line by line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+fn parse_input() -> Result<(), io::Error> {
+    let file = try!(File::open("./test_data/test_00.input"));
+    let reader = BufReader::new(&file);
+    for line in reader.lines() {
+        let l = line.unwrap();
+        println!("{}", l); 
+    }               
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x = parse_input();
+    match x {
+        Err(c) => println!("Parsing error: {}", c),
+        Ok(_) => println!("Parsing OK")
+    }
 }
