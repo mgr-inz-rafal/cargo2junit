a14016e5317a06e050091b0f924d8fa2ed639e40
mgr_inz_rafal <rchabowski@gmail.com>
2018-08-14 23:04:31 +0200

    Introducing state machine

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,45 @@
-use std::io;
+extern crate regex;
+
+use regex::Regex;
 use std::fs::File;
+use std::io;
 use std::io::BufRead;
 use std::io::BufReader;
 
-fn parse_input() -> Result<(), io::Error> {
+#[derive(Debug)]
+enum States {
+    Undefined,
+    ScanningForUnitTests,
+    ParsingUnitTests,
+    ScanningForDocTests,
+    ParsingDocTests,
+    Finished,
+}
+
+fn set_state(old_state: &mut States, new_state: States) {
+    println!("Machine state change: {:?} -> {:?}", old_state, new_state);
+    *old_state = new_state;
+}
+
+fn parse_input(mut state: &mut States) -> Result<(), io::Error> {
     let file = try!(File::open("./test_data/test_00.input"));
     let reader = BufReader::new(&file);
+    set_state(&mut state, States::ScanningForUnitTests);
+
     for line in reader.lines() {
         let l = line.unwrap();
-        println!("{}", l); 
-    }               
+        match state {
+            _ => println!("{}", l),
+        }
+    }
     Ok(())
 }
 
 fn main() {
-    let x = parse_input();
+    let mut state = States::Undefined;
+    let x = parse_input(&mut state);
     match x {
         Err(c) => println!("Parsing error: {}", c),
-        Ok(_) => println!("Parsing OK")
+        Ok(_) => println!("Parsing OK"),
     }
 }
